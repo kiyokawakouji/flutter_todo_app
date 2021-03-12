c8e05bc6eb618e6e9fad9fd1eb954e8c2f8645e6
kiyokawakouji <kohji12k@gmail.com>
2021-03-12 20:01:21 +0900

    todolist入力フォーム作成

--- lib/todo_add_page.dart
+++ lib/todo_add_page.dart
@@ -1,16 +1,38 @@
+//import 'dart:js';
+
 import 'package:flutter/material.dart';
+
 
 class TodoAddPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Center(
-        child: FlatButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text('リストに追加する'),
-        ),
+      body: Container(
+        padding: EdgeInsets.all(64),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(),
+            Container(
+              width: screenWidth * 1,
+              child: RaisedButton(
+                color: Colors.blue,
+                onPressed: () {},
+                child: Text('リスト追加', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            Container(
+              width: screenWidth * 1,
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              child: Text('キャンセル'),
+              ),
+            )
+          ],
+        )
       ),
     );
   }
